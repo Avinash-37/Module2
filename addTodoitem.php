dd28402f9f386325590e28121d7a83ad0185f0b4
Avinash-37 <42609152+Avinash-37@users.noreply.github.com>
2018-10-23 18:43:12 +0530

    Add files via upload

---
+++ addTodoitem.php
@@ -0,0 +1,21 @@
+<?php
+include 'dbconfig.php';
+session_start();
+if(!$_SESSION["E_MAIL"])
+	{
+	header("Location:index.html");
+	}
+	else
+	{
+	$user=$_SESSION["E_MAIL"];
+	$dataList=$_REQUEST["myInput"];
+	//$dataList="HTML";
+	//$user ="avinash";
+	$date = date('Y-m-d H:i:s');
+
+	mysqli_query($conn,"INSERT INTO `todotable` (`list`, `todoDate`, `uploadedBy`) VALUES ('$dataList', '$date', '$user');");
+
+	header("Location:todolist.html");
+	}
+
+?>
